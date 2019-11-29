3970d58edbc7bf4ba8971129cdbd23a2699bfa74
Steve Morrow <smorrow@golubcapital.com>
2019-11-29 10:19:13 -0600

    Create file share

--- PSSetup.ps1
+++ PSSetup.ps1
@@ -8,7 +8,29 @@ New-AzResourceGroup -Name $RESOURCE_GROUP -Location $LOCATION -Verbose
 
 $suffix = $(Get-Random -Minimum 10000 -Maximum 99999)
 
-$storageacctname = "granny$suffix"
-Write-Host "`nCreating a new resource group: $RESOURCE_GROUP ... (1/?)"
-New-AzStorageAccount -ResourceGroupName $RESOURCE_GROUP -Name $storageacctname -Location $LOCATION -SkuName Standard_LRS
+$staccname = "granny$suffix"
+Write-Host "`nCreating a new storage account: $staccname ... (2/?)"
+New-AzStorageAccount `
+   -ResourceGroupName $RESOURCE_GROUP `
+   -Location $LOCATION `
+   -Name $staccname `
+   -SkuName Standard_LRS
+
+$stsharname = "grannyshare$suffix"
+Write-Host "`nCreating a new storage share: $stsharname ... (3/?)"
+$stacckey = (Get-AzStorageAccountKey `
+    -ResourceGroupName $RESOURCE_GROUP `
+    -Name $staccname)[0].Value
+$stctx = New-AzStorageContext `
+    -StorageAccountName $staccname `
+    -StorageAccountKey $stacckey
+New-AzStorageShare `
+    -Name $stsharname `
+    -Context $stctx
+Set-AzStorageShareQuota `
+    -ShareName $stsharname `
+    -Quota 250 `
+    -Context $stctx
+
+
 
