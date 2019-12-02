4d0a18df41631a83de09e1bafc9de4679752248e
Steve Morrow <steve@yawmark.com>
2019-12-02 11:41:53 -0600

    Create netcore 2.2 app with dir browsing

---
+++ mcweb/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace mcweb
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        #region snippet_ConfigureMethod
+        public void Configure(IApplicationBuilder app)
+        {
+            // app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseDirectoryBrowser();
+            // app.UseDirectoryBrowser(new DirectoryBrowserOptions
+            // {
+            //     FileProvider = new PhysicalFileProvider(
+            //         Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images")),
+            //         RequestPath = "/MyImages"
+            // });
+
+            // app.Run(async (context) =>
+            // {
+            //     await context.Response.WriteAsync("Hello World!");
+            // });
+        }
+        #endregion
+    }
+}
