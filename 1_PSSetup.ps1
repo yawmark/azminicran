e1871fb27e54e81de29a465e68faee636eab0e4c
Steve Morrow <smorrow@golubcapital.com>
2019-12-03 15:17:43 -0600

    Converted PS to CLI

--- 1_PSSetup.ps1
+++ 1_PSSetup.ps1
@@ -6,47 +6,32 @@ $RESOURCE_GROUP = "trg-$PNAME"
 $LOCATION = "northcentralus"
 
 Write-Host "`nCreating a new resource group: $RESOURCE_GROUP ... (1/?)`n`n"
-New-AzResourceGroup -Name $RESOURCE_GROUP -Location $LOCATION -Verbose
+az group create -g $RESOURCE_GROUP -l $LOCATION
 
 $suffix = $(Get-Random -Minimum 10000 -Maximum 99999)
 
 $staccname = "${PNAME}stacc$suffix"
 Write-Host "`nCreating a new storage account: $staccname ... (2/?)`n`n"
-New-AzStorageAccount `
-   -ResourceGroupName $RESOURCE_GROUP `
-   -Location $LOCATION `
-   -Name $staccname `
-   -SkuName Standard_LRS
+az storage account create -n $staccname -g $RESOURCE_GROUP -l $LOCATION --sku Standard_LRS
 
 $stsharname = "${PNAME}share$suffix"
 Write-Host "`nCreating a new storage share: $stsharname ... (3/?)`n`n"
-$stacckey = (Get-AzStorageAccountKey `
-    -ResourceGroupName $RESOURCE_GROUP `
-    -Name $staccname)[0].Value
-$stctx = New-AzStorageContext `
-    -StorageAccountName $staccname `
-    -StorageAccountKey $stacckey
-New-AzStorageShare `
-    -Name $stsharname `
-    -Context $stctx
-Set-AzStorageShareQuota `
-    -ShareName $stsharname `
-    -Quota 250 `
-    -Context $stctx
+$stacckey = az storage account keys list --account-name $staccname --query "[?keyName=='key1'].{value:value}" -o tsv
+az storage share create -n $stsharname --account-name $staccname --account-key $stacckey --quota 250 --verbose
 
-$aspname = "${PNAME}asp$suffix"
-Write-Host "`nCreating a new app service plan: $aspname ... (4/?)`n`n" 
-New-AzAppServicePlan `
-    -Name $aspname `
-    -ResourceGroupName $RESOURCE_GROUP `
-    -Location $LOCATION `
-    -Tier Free
+# $aspname = "${PNAME}asp$suffix"
+# Write-Host "`nCreating a new app service plan: $aspname ... (4/?)`n`n" 
+# New-AzAppServicePlan `
+#     -Name $aspname `
+#     -ResourceGroupName $RESOURCE_GROUP `
+#     -Location $LOCATION `
+#     -Tier Free
 
-$webappname = "${PNAME}web$suffix"
-Write-Host "`nCreating a new web app: $webappname ... (5/?)`n`n"
-New-AzWebApp `
-    -Name $webappname `
-    -ResourceGroupName $RESOURCE_GROUP `
-    -Location $LOCATION `
-    -AppServicePlan $aspname `
-    -GitRepositoryPath mcweb
+# $webappname = "${PNAME}web$suffix"
+# Write-Host "`nCreating a new web app: $webappname ... (5/?)`n`n"
+# New-AzWebApp `
+#     -Name $webappname `
+#     -ResourceGroupName $RESOURCE_GROUP `
+#     -Location $LOCATION `
+#     -AppServicePlan $aspname `
+#     -GitRepositoryPath mcweb
